573913a9c5b752630f573ca0a151097170894b9c
hMatoba <hiroaki.mtb@outlook.com>
2017-06-27 08:19:59 +0900

    Initialize

---
+++ Mango/Mango/Mango.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Linq;
+
+namespace Mango
+{
+    public class MongoInitializer
+    {
+        public static void Initialize(string assemblyName, string namespaceName="")
+        {
+            var namespaceFullName = namespaceName == "" ? $"{assemblyName}.Models" : namespaceName;
+            var myAssembly = Assembly.Load(new AssemblyName(assemblyName));
+            var myTypes = myAssembly.GetTypes().Where(t => t.Namespace == namespaceFullName).ToList();
+            foreach (var type in myTypes)
+            {
+                Console.WriteLine(type.Name);
+                var properties = type.GetProperties();
+                foreach (var property in properties)
+                {
+                    var attributes = property.GetCustomAttributes();
+                    Console.WriteLine(String.Join(", ", attributes));
+                }
+            }
+        }
+    }
+}
---
+++ Mango/RunMango/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Mango;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RunMango
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoInitializer.Initialize("RunMango");
+        }
+    }
+}
+
+namespace RunMango.Models
+{
+    class Model1
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public string Id { get; set; }
+
+        [BsonElement("token")]
+        [BsonRepresentation(BsonType.String)]
+        public string Token { get; set; }
+    }
+
+    class Model2
+    {
+
+    }
+}
