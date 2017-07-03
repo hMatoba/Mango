d4e9d8e3508732398dd762664b5a85c990035192
hMatoba <hiroaki.mtb@outlook.com>
2017-07-03 23:05:52 +0900

    Refactoring

---
+++ MangoTests/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MangoTests
+{
+    static class Settings
+    {
+        public static string connectionString = "mongodb://127.0.0.1";
+        public static string dbName = "mango";
+        public static string assemblyName = "MangoTests";
+    }
+}
